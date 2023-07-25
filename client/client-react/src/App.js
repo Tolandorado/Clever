2b67aa61b777f93669c75ee7c84f66563a74a6ca
Tolandorado <tonisagaz@mail.ru>
2023-07-25 13:17:03 +0300

    client

--- client/client-react/src/App.js
+++ client/client-react/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { RoutesMain } from './components/Routes/routes';
-import { Wrapper } from './components/Wrapper/wrapper'
+import { Wrapper } from './components/Wrapper/wrapper';
+import { createContext } from 'react';
 
 function App() {
   return (
