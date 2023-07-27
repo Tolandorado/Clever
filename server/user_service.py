c8811425cb9e99ad8df0186cebc769646e753bd3
Andrew <authordriu@yandex.ru>
2023-07-27 14:20:38 +0300

    improved id (now hex)

--- server/user_service.py
+++ server/user_service.py
@@ -7,16 +7,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://klever:1@192.168.1.72:5432
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.String(50), unique=True)
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
     
     def __repr__(self):
-        return f"<User id:{self.id} name:\"{self.username}\">"
+        return f"<User id:{self.user_id} name:\"{self.username}\">"
     
 
 @app.after_request
@@ -25,7 +28,7 @@ def after_request(response):
     return response
 
 # Создаёт пользователя
-@app.route("/users/api/create", methods=["POST",])
+@app.route("/api/users/create", methods=["POST",])
 def create_user():
     new_user = User(request.form["username"], request.form["password"])
     try:
@@ -36,46 +39,46 @@ def create_user():
         print(ex)
         print("Не удалось создать пользователя", new_user)
         return jsonify({
-            "responce-suc": False
+            "response-suc": False
         })
     else:
+        new_user.user_id = hex(new_user.id)[2::]
         db.session.commit()
         print("Создан пользователь", new_user)
         return jsonify({
-            "responce-suc": True
+            "response-suc": True
         })
 
 # Возвращает список пользователей
-@app.route("/users/api/get", methods=["GET",])
+@app.route("/api/users/get", methods=["GET",])
 def get_users():
     try:
         users = User.query.all()
     except Exception as ex:
         print(ex)
         return jsonify({
-            "responce-suc": False
+            "response-suc": False
         })
     else:
         if users is None: return jsonify({
-            "responce-suc": True,
+            "response-suc": True,
             "users": None
         })
         for i in range(len(users)): users[i] = {
-            "id": users[i].id if users[i] is not None else None,
+            "id": users[i].user_id if users[i] is not None else None,
             "username": users[i].username if users[i] is not None else None
         }
         users = {
-            "responce-suc": True,
+            "response-suc": True,
             "users": users
         }
         return jsonify(users)
 
 # Возвращает пользователя по id
-@app.route("/users/api/get/<string:user_id>")
+@app.route("/api/users/get/<string:user_id>")
 def get_user_by_id(user_id):
-    user_id = int(user_id, 16)
     try:
-        user = User.query.get(user_id)
+        user = db.session.query(User).filter_by(user_id=user_id).one()
     except Exception as ex:
         print(ex)
         return jsonify({
@@ -83,34 +86,33 @@ def get_user_by_id(user_id):
         })
     else:
         return jsonify({
-            "responce-suc": True,
+            "response-suc": True,
             "username": user.username if user is not None else None,
             "password": user.password if user is not None else None
         })
 
 # Удаляет пользователя с подходящим id из базы данных
-@app.route("/users/api/delete/<string:user_id>", methods=["DELETE",])
+@app.route("/api/users/delete/<string:user_id>", methods=["DELETE",])
 def delete_user(user_id):
-    user_id = int(user_id, 16)
     try:
-        User.query.filter_by(id=user_id).delete()
+        User.query.filter_by(user_id=user_id).delete()
         db.session.flush()
     except Exception as ex:
         db.session.rollback()
         print(ex)
         return jsonify({
-            "responce-suc": False
+            "response-suc": False
         })
     else:
         db.session.commit()
         return jsonify({
-            "responce-suc": True
+            "response-suc": True
         })
 
 
 # Ищёт в бд пользователя с указанных именем и проверяет пароль на соответствие.
 # В случае отстутствия пользователя с таким именем, или несоотвествия пароля в correct будет выведено False
-@app.route("/users/api/verify", methods=["GET", "POST"])
+@app.route("/api/users/verify", methods=["GET", "POST"])
 def verify_user():
     if request.method == "GET":
         username = request.args["username"]
@@ -124,13 +126,13 @@ def verify_user():
     except Exception as ex:
         print(ex)
         return jsonify({
-            "responce-suc": False
+            "response-suc": False
         })
     else:
         return jsonify({
-            "responce-suc": True,
+            "response-suc": True,
             "correct": False if user is None or user.password != password else True,
-            "id": user.id if user is not None and user.password == password else None
+            "id": user.user_id if user is not None and user.password == password else None
         })
 
 if __name__ == "__main__":
