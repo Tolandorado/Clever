0ad442ded04c059d748e0ff07316024459c776ce
NiLNight <bkmz-2020@inbox.ru>
2023-08-04 17:42:32 +0300

    gjabu

--- Post server/main.py
+++ Post server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,44 +13,59 @@ class Post(db.Model):
     path = db.Column(db.String(200), nullable=False)
     author_id = db.Column(db.String(50), nullable=False)
 
-
-def __repr__(self):
-    return f"Post(name='{self.name}', post_id='{self.post_id}', path='{self.path}', author_id='{self.author_id}')"
+    def __repr__(self):
+        return f"<Post id:{self.post_id}>"
 
 
 @app.route('/api/post/create/', methods=['POST'])
 def create_post():
-    data = request.json
-    new_post = Post(name=data['name'], post_id=data['post_id'], path=data['path'], author_id=data['author_id'])
-    db.session.add(new_post)
-    db.session.commit()
-    return f"Post {new_post.id} has been created successfully."
+    try:
+        data = request.json
+        new_post = Post(name=data['name'], post_id=data['post_id'], path=data['path'], author_id=data['author_id'])
+        db.session.add(new_post)
+        db.session.commit()
+        return jsonify({'message': f"Post {new_post.id} has been created successfully."}), 201
+    except KeyError:
+        return jsonify({'error': 'Invalid data provided.'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/api/post/read/<int:post_id>/', methods=['GET'])
 def read_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    return f"Post {post.id}: {post.name}, {post.post_id}, {post.path}, {post.author_id}"
+    try:
+        post = Post.query.get_or_404(post_id)
+        return jsonify({'id': post.id, 'name': post.name, 'post_id': post.post_id, 'path': post.path, 'author_id': post.author_id})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/api/post/update/<int:post_id>/', methods=['PUT'])
 def update_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    data = request.json
-    post.name = data['name']
-    post.post_id = data['post_id']
-    post.path = data['path']
-    post.author_id = data['author_id']
-    db.session.commit()
-    return f"Post {post.id} has been updated successfully."
+    try:
+        post = Post.query.get_or_404(post_id)
+        data = request.json
+        post.name = data['name']
+        post.post_id = data['post_id']
+        post.path = data['path']
+        post.author_id = data['author_id']
+        db.session.commit()
+        return jsonify({'message': f"Post {post.id} has been updated successfully."}), 200
+    except KeyError:
+        return jsonify({'error': 'Invalid data provided.'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/api/post/delete/<int:post_id>/', methods=['DELETE'])
 def delete_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    db.session.delete(post)
-    db.session.commit()
-    return f"Post {post.id} has been deleted successfully."
+    try:
+        post = Post.query.get_or_404(post_id)
+        db.session.delete(post)
+        db.session.commit()
+        return jsonify({'message': f"Post {post.id} has been deleted successfully."}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
