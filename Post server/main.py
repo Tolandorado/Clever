2db2933ccc4d8f6b953200a09497635aa1d62c46
NiLNight <bkmz-2020@inbox.ru>
2023-08-09 19:52:18 +0300

    ура все работает

--- Post server/main.py
+++ Post server/main.py
@@ -41,10 +41,25 @@ def create_post():
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/api/post/read/<int:post_id>/', methods=['GET'])
-def read_post(post_id):
+@app.route('/api/post/range/<vector>/s<int:start>/s<int:end>/', methods=['GET'])
+def get_posts_range(vector, start, end):
     try:
-        post = Post.query.get_or_404(post_id)
+        table = get_table_for_vector(vector)
+        if table is None:
+            return jsonify({'error': 'Invalid vector provided.'}), 400
+        posts = table.query.filter(table.id >= start, table.id <= end).all()
+        return jsonify([post.to_dict() for post in posts])
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/api/post/read/<vector>/<int:post_id>/', methods=['GET'])
+def read_post(vector, post_id):
+    try:
+        table = get_table_for_vector(vector)
+        if table is None:
+            return jsonify({'error': 'Invalid vector provided.'}), 400
+        post = table.query.get_or_404(post_id)
         return jsonify(post.to_dict())
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -53,63 +68,35 @@ def read_post(post_id):
 @app.route('/api/post/update/<int:post_id>/', methods=['PUT'])
 def update_post(post_id):
     try:
-        post = Post.query.get_or_404(post_id)
         data = request.json
-        vector = data.get('vector')
-        post_type = data.get('post_type')
+        vector = data['vector']
+        table = get_table_for_vector(vector)
+        post = table.query.get_or_404(post_id)
+        if table is None:
+            return jsonify({'error': 'Invalid vector provided.'}), 400
 
-        if vector and post_type:
-            table = get_table_for_vector(vector)
-            if table:
-                for key, value in data.items():
-                    setattr(post, key, value)
-                db.session.commit()
-                return jsonify({'message': f"Post {post.id} has been updated successfully."}), 200
-            else:
-                return jsonify({'error': 'Invalid vector provided.'}), 400
-        else:
-            return jsonify({'error': 'Invalid vector or post_type provided.'}), 400
+        post.name = data['name']
+        post.post_id = data['post_id']
+        post.path = data['path']
+        post.author_id = data['author_id']
+        db.session.commit()
+        return jsonify({'message': f"Post {post.id} has been updated successfully."}), 200
     except KeyError:
         return jsonify({'error': 'Invalid data provided.'}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/api/post/list/', methods=['GET'])
-def get_post_list():
+@app.route('/api/post/delete/<vector>/<int:post_id>/', methods=['DELETE'])
+def delete_post(vector, post_id):
     try:
-        vectors = request.args.getlist('vectors')
-        types = request.args.getlist('types')
-
-        query = db.session.query()
-
-        for vector in vectors:
-            table = get_table_for_vector(vector)
-            if table:
-                query = query.union_all(db.session.query(table))
-
-        if types:
-            query = query.filter(Post.post_type.in_(types))
-
-        posts = query.all()
-        post_list = [post.to_dict() for post in posts]
-        return jsonify(post_list)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-@app.route('/api/post/delete/<int:post_id>/', methods=['DELETE'])
-def delete_post(post_id):
-    try:
-        post = Post.query.get_or_404(post_id)
-        table = get_table_for_vector(post.vector)
-        if table:
-            post_from_table = table.query.get_or_404(post_id)
-            db.session.delete(post_from_table)
-            db.session.commit()
-            return jsonify({'message': f"Post {post.id} has been deleted successfully."}), 200
-        else:
+        table = get_table_for_vector(vector)
+        if table is None:
             return jsonify({'error': 'Invalid vector provided.'}), 400
+        post = table.query.get_or_404(post_id)
+        db.session.delete(post)
+        db.session.commit()
+        return jsonify({'message': f"Post {post.id} has been deleted successfully."}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
