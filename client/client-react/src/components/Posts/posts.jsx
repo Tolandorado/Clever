774fe66ac1ee378a7eab1063a50a108a40ea4e5d
Vlad <VladM19082006@gmail.com>
2023-08-31 21:32:19 +0300

    posts

--- client/client-react/src/components/Posts/posts.jsx
+++ client/client-react/src/components/Posts/posts.jsx
@@ -2,6 +2,9 @@ import { NavLink } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../../AuthContext";
+import { PostCreate } from "../PostCreate/postCreate";
+import styles from "./posts.module.scss";
+import { Link } from "react-router-dom";
 
 export const Posts = () => {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
@@ -38,20 +41,68 @@ export const Posts = () => {
   //     <NavLink to="/logined">Go to Main page</NavLink>
   //   </div>
   // );
-  if (isLoggedIn === false) {
-    return (
-      <div>
-        <h1>post</h1>
-        <NavLink to="/logined">Пройдите авторизацию.</NavLink>
-      </div>
-    );
-  }
+
+  const [pages, setPages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fetching, setFetching] = useState(true);
+  const [totalCount, setTotalCount] = useState(0);
+
+  useEffect(() => {
+    if (fetching) {
+      console.log("fetching");
+      axios
+        .get(
+          `https://jsonplaceholder.typicode.com/photos?_limit=40&_page=${currentPage}`
+        )
+        .then((response) => {
+          setPages([...pages, ...response.data]);
+          setCurrentPage((prevState) => prevState + 1);
+          setTotalCount(response.headers["x-total-count"]);
+        })
+        .finally(() => setFetching(false));
+    }
+  }, [fetching]);
+
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHandler);
+    return function () {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, [totalCount]);
+
+  const scrollHandler = (e) => {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+        100 &&
+      pages.length < totalCount
+    ) {
+      setFetching(true);
+    }
+  };
 
   return (
-    <div>
-      <h1>post</h1>
+    <div className="main">
+      <h1>Posts</h1>
+      <div className={styles.postContainer}>
+        {pages.map((page) => (
+          <div key={page.id} className={styles.postCard}>
+            <img
+              src={page.thumbnailUrl}
+              alt={page.title}
+              className={styles.postImage}
+            />
+            <div className={styles.postContent}>
+              <h2 className={styles.postTitle}>{page.title}</h2>
+              <p className={styles.postDate}>
+                {new Date(page.date).toLocaleDateString()}
+              </p>
+              <p className={styles.postAuthor}>By {page.author}</p>
+            </div>
+          </div>
+        ))}
+      </div>
       <NavLink to="/logined">Go to Main page</NavLink>
-      <NavLink to="/postCreate">Create post</NavLink>
     </div>
   );
 };
